fe2f7c55b32b041e629efc85af7649e4ab5912d2
Malte <Malte@10.50.138.127>
2018-11-09 16:13:49 +0100

    TEST

--- Delfinen/src/delfinen/Delfinen.java
+++ Delfinen/src/delfinen/Delfinen.java
@@ -15,7 +15,7 @@ public class Delfinen {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // git 
     }
     
 }
