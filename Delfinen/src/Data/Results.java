ef293279ba40c1974e1dcec6e65097eec1da89a3
Nikolaj <Nikolaj@10.50.138.244>
2018-11-13 12:31:15 +0100

    Træner Indberet Resultater Træning og Konkurrence

--- Delfinen/src/Data/Results.java
+++ Delfinen/src/Data/Results.java
@@ -5,6 +5,11 @@
  */
 package Data;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  *
  * @author mikkel
@@ -12,6 +17,50 @@ package Data;
 public class Results {
     // Swimming results.
     // There exists Competitive results and Training results.
+    
+    public void registerKResults(int id, int distance, double time, String date) {
+        DBConnector conn = null;
+        Connection connection;
+        String query = "";
+        ResultSet rs = null;
+
+        try {
+            conn = new DBConnector();
+            query = "INSERT INTO `delfinen`.`konkurrence_results` VALUE (`"+id+"`, `"+distance+"`, `"+time+"`, `"+date+"`);";
+            connection = conn.getConnection();
+            Statement stmt = connection.createStatement();
+            rs = stmt.executeQuery(query);
+            
+
+        } catch (SQLException ex) {
+
+        } catch (Exception ex) {
+            
+            
+        }
+    }
+    
+    public void registerTResults(int id, String competition, String placement, double time) {
+        DBConnector conn = null;
+        Connection connection;
+        String query = "";
+        ResultSet rs = null;
+
+        try {
+            conn = new DBConnector();
+            query = "INSERT INTO `delfinen`.`trainnings_results` VALUE (`"+id+"`, `"+competition+"`, `"+placement+"`, `"+time+"`);";
+            connection = conn.getConnection();
+            Statement stmt = connection.createStatement();
+            rs = stmt.executeQuery(query);
+            
+
+        } catch (SQLException ex) {
+
+        } catch (Exception ex) {
+            
+            
+        }
+    }
     
     
 }
