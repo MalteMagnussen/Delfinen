1bd100b26a2c730527e0b83074594faed4a727e2
Malte <Malte@10.50.138.127>
2018-11-14 11:42:21 +0100

    format

--- Delfinen/src/Data/DBConnector.java
+++ Delfinen/src/Data/DBConnector.java
@@ -9,23 +9,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnector {
+
     private Connection connection = null;
-	
+
     //Constants
-    private static final String IP	 = "localhost"; 
-    private static final String PORT     = "8889";
+    private static final String IP = "localhost";
+    private static final String PORT = "8889";
     public static final String DATABASE = "delfinen";
-    private static final String USERNAME = "root"; 
-    private static final String PASSWORD = "root";	
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
 
-
-	
     public DBConnector() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
         String url = "jdbc:mysql://" + IP + ":" + PORT + "/" + DATABASE + "?";
-        
-        this.connection = (Connection) DriverManager.getConnection(url + 
-                "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", USERNAME, PASSWORD);
+
+        this.connection = (Connection) DriverManager.getConnection(url
+                + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC", USERNAME, PASSWORD);
     }
 
     public Connection getConnection() {
