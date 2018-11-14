5261f69cb29221845030da88e3b5db84c607a4d3
Malte <Malte@10.50.138.127>
2018-11-14 11:39:38 +0100

    format

--- Delfinen/src/textreader/TextReader.java
+++ Delfinen/src/textreader/TextReader.java
@@ -18,6 +18,8 @@ public class TextReader {
     /**
      *
      * @param filePath
+     * @return 
+     * @throws java.io.FileNotFoundException
      */
     public static String textReader(String filePath) throws FileNotFoundException, IOException {
         File path = new File(filePath);
