23b7d28c2a87d61c7957a497d74e9f04808b83a9
benja <benja_kongs@hotmail.com>
2018-11-14 08:41:22 +0100

    create player

---
+++ Delfinen/src/textreader/FileReplace.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package textreader;
+
+import java.io.IOException;
+
+public class FileReplace {
+
+    public void fileReplace(String oldFilePath, String newFilePath, String findWord, String replaceWord) {
+        String text = "";
+        TextReader tr = new TextReader();
+        try {
+            text = TextReader.textReader(oldFilePath);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        
+        String newText = text.replaceAll(findWord, replaceWord);
+        TextWriter tw = new TextWriter();
+        
+        
+        try {
+            tw.textWriter(newFilePath, newText);
+
+        } catch (IOException e) {
+            e.getMessage();
+        }
+    }
+
+}
