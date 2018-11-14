6c4721a6fe27fefae71011c44c3b69abe81da852
Malte <Malte@10.50.138.127>
2018-11-14 13:12:40 +0100

    restance

--- Delfinen/src/Data/Member.java
+++ Delfinen/src/Data/Member.java
@@ -2,6 +2,7 @@ package Data;
 
 import Logic.DataAccessor;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,21 +16,23 @@ public class Member implements Swimmer {
     private LocalDate birthday;
     private String ID;
     private boolean status;
-    private LocalDate joinDate;
-
-    Map<String, Boolean> payment = new HashMap<>();
+    private int joinDate;
+    ArrayList<Integer> payment = new ArrayList<>();
 
     public Member(String name, LocalDate birthday, boolean status, char MK) {
         this.name = name;
         this.birthday = birthday;
         this.status = status;
         this.ID = "" + MK + createID(); // Assigns ID.
-        // set payment map.
+        this.joinDate = LocalDate.now().getYear();
+        initializePayments();
     }
 
     public Member(String name, int age, boolean status) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+    
+    
 
     // Returns the Age, not the Birthday.
     @Override
@@ -68,24 +71,23 @@ public class Member implements Swimmer {
     }
 
     @Override
-    public LocalDate getYearJoined() {
+    public int getYearJoined() {
         return this.joinDate;
     }
 
     public LocalDate getBirthday() {
         return birthday;
     }
+    
+    private void initializePayments() {
+        int clubStartDate = 2010;
+        for (int i = 0; i < joinDate-clubStartDate ; i++){
+            payment.add(clubStartDate + i);
+        }
+    }
 
     public boolean isStatus() {
         return status;
-    }
-
-    public LocalDate getJoinDate() {
-        return joinDate;
-    }
-
-    public Map<String, Boolean> getPayment() {
-        return payment;
     }
 
     private void setPaymentMap() {
