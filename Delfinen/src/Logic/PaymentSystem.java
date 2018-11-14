c107b0153537ec66cb5173747f26c0bbecbba952
Nikolaj <Nikolaj@10.50.138.244>
2018-11-14 14:32:36 +0100

    Kassere Vis Restance Made in the PaymentSystem Class

--- Delfinen/src/Logic/PaymentSystem.java
+++ Delfinen/src/Logic/PaymentSystem.java
@@ -10,7 +10,11 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  *
@@ -49,8 +53,40 @@ public class PaymentSystem {
 
     }
 
-    public void getAllPlatersPayment() {
-
+    public ArrayList getAllPlayersPayment() throws JSONException{
+        // Pull's the id Json File
+        JSONObject obj2 = new JSONObject(JSON_DATA);
+        JSONArray ids = obj2.getJSONArray("id's");
+        int idsLen = ids.length();
+        // Pull's Data From Json about the join Json Array
+        JSONObject obj = new JSONObject(JSON_DATA);
+        JSONArray  allYears = obj.getJSONArray("allYears");
+        int years = 0;
+        // Pull's the members from JSON File
+        JSONObject obj3 = new JSONObject(JSON_DATA);
+        JSONArray members = obj3.getJSONArray("members");
+        // Array to put the members in who is missing to pay
+        ArrayList notPaid = new ArrayList();
+        // Loop to find and get the data from Json
+        for(int i = 0; i == idsLen; ++i) {
+            // Pulls the local year
+            LocalDate currentYear = LocalDate.now();
+            int year = currentYear.getYear();
+            // The year the club was made
+            int clubCele = 2010;
+            // Gets the years since the club was made
+            int clubInYears = year - clubCele;
+            // Integer to get the JSON's length of all the years
+            years = allYears.length();
+            
+            // Loop to see if the member has paid or not
+            if(clubInYears - years < 0) {
+                // Takes the member whith the id and stores it in an ArrayList
+                notPaid.add(members.getJSONObject(i));
+            }
+        }
+        // Spits out the Members that have not Paid
+        return notPaid;
     }
 
 }
