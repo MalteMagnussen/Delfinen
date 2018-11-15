c53fe426b8e7d25f27ba4a5c68ce5557e1338d98
Malte <Malte@DESKTOP-8PCQKCC.home>
2018-11-15 20:33:26 +0100

    TxtAccess stuff.

--- Delfinen/src/Data/TxtAccess.java
+++ Delfinen/src/Data/TxtAccess.java
@@ -6,15 +6,12 @@
 package Data;
 
 import Logic.Member;
-import Logic.Controller;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
