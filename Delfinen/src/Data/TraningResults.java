0033db0f56baaa4033c016cea75ea8d4a3b8e00e
benja <benja_kongs@hotmail.com>
2018-11-16 12:46:15 +0100

    Merge origin/master
    Conflicts:
    	Delfinen/ID.txt
    	Delfinen/members.txt
    	Delfinen/src/Logic/PaymentSystem.java

---
+++ Delfinen/src/Data/TraningResults.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Data;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author kemokongshaug
+ */
+public class TraningResults {
+    
+    private String id;
+    private int distance;
+    private double time;
+    private LocalDate date;
+ 
+    
+    // Part of the Coach Registrer Trainning Results, rest can be found in Controller
+    public void registerTResults(String id, int distance, double time, LocalDate date) {
+        this.id = id;
+        this.distance = distance;
+        this.time = time;
+        this.date = date;
+    
+}
+    
+}
