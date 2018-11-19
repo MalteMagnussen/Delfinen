8afa8fdb588ea5947bb4d3bc462f1d8591e915a0
Nikolaj <Nikolaj@10.50.138.244>
2018-11-19 13:17:59 +0100

    Tests

--- Delfinen/src/Data/TxtAccess.java
+++ Delfinen/src/Data/TxtAccess.java
@@ -424,4 +424,19 @@ public class TxtAccess {
         }
         return jors;
     }
+    
+    public void deleteTraningsResults() {
+        try {
+        String total = "";
+            File file = new File(TraningResultsPath);
+            Scanner s = new Scanner(new BufferedReader(new FileReader(file)));
+
+            do {
+                String next = s.next();
+                total += "";
+            } while(s.hasNext());
+        } catch(Exception e) {
+            
+        }
+    }
 }
