b89be1015491cbd2cf9ac007463a9868b4ec4ea9
Nikolaj <Nikolaj@10.50.138.244>
2018-11-21 10:17:15 +0100

    Tests

--- Delfinen/test/Logic/ControllerTest.java
+++ Delfinen/test/Logic/ControllerTest.java
@@ -3,6 +3,7 @@ package Logic;
 
 import Data.TxtAccess;
 import java.time.LocalDate;
+import java.time.Month;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -19,6 +20,8 @@ public class ControllerTest {
     
     Controller cont = new Controller();
     TxtAccess acc = new TxtAccess();
+    
+    List<Competition> comps;
     
     private int before = 0;
     private int post = 0;
@@ -46,6 +49,9 @@ public class ControllerTest {
         cont.makeMember("N9", LocalDate.of(1994, 03, 02), "hed", "gah", "1325", true, "Crawl");
         cont.makeMember("N10", LocalDate.of(1994, 03, 02), "hed", "gah", "1325", true, "Crawl");
         post = acc.getHighestID();
+        
+        // Part of the Test to registrer Competition 
+        cont.competitionRegistrer("OLYMPIC", LocalDate.of(2001, Month.MARCH, 15));
     }
     
     @After
@@ -72,13 +78,28 @@ public class ControllerTest {
     @Test
     public void negativeFindTopFiveTest() {
         String[] test;
-        cont.MakeTrainingResult("1", 100, 0.0, LocalDate.now());
-        cont.MakeTrainingResult("2", 100, -1, LocalDate.now());
-        cont.MakeTrainingResult("3", 100, -1.4, LocalDate.now());
-        cont.MakeTrainingResult("4", 100, -4.1, LocalDate.now());
-        cont.MakeTrainingResult("5", 100, 0.1, LocalDate.now());
+        try {
         cont.MakeTrainingResult("6", 100, -4, LocalDate.now());
         test = cont.FindTopFiveId(100);
         System.out.println(Arrays.toString(test));
+        } catch(Exception e) {
+            // Expected
+        }
+    }
+    
+    // Test Of Registrrer Competition in controller and getCompetition in TxtAccess
+    @Test
+    public void testCompetitionRegistrer() {
+        try {
+            comps = acc.getCompetitions();
+            for(int i = 0; i < comps.size(); ++i) {
+                if(comps.get(i).getName().equals("OLYMPIC") && comps.get(i).getLocalDate().equals(LocalDate.of(2001, Month.MARCH, 15))){
+                    return;
+                }
+            }
+        } catch(Exception e) {
+            // NOT EXPECTED 
+            System.out.println("Excepion");
+        }
     }
 }
